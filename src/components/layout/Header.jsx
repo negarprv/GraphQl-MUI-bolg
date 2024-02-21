5404082ee7172a3b0596a5806b4d871be4554b10
Negar Parvaresh <negar.prv@gmail.com>
2024-02-21 20:26:04 +0330

    Navbar compeleted

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,154 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const pages = ["News", "Blogs", "About", "Contact"];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState();
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        overflow: "hidden",
+        borderRadius: "10px",
+        backgroundColor: " transparent",
+        top: "10px",
+        width: "90%",
+        margin: "0px auto",
+        boxShadow: "none",
+      }}
+    >
+      <Container
+        maxWidth="xl"
+        sx={{
+          borderRadius: "10px",
+          backgroundColor: "#3a4660",
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              textDecoration: "none",
+              color: "#c9af98",
+            }}
+          >
+            NegarBlog
+          </Typography>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "end",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "#FAF9F6",
+                  display: "block",
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              justifyContent: "end",
+              color: "#c9af98",
+              textDecoration: "none",
+            }}
+          >
+            NegarBlog
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api-ca-central-1.hygraph.com/v2/clsvrl0sj0x4m07w3xcista93/master",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>
+);
